571e3abe5e3fced45e46518e1e185d8b9e99df08
sergeychaliy2 <70662544+sergeychaliy2@users.noreply.github.com>
2021-04-01 20:23:58 +0300

    Update threader.c

--- lab03/threader.c
+++ lab03/threader.c
@@ -12,8 +12,6 @@ return NULL;
 }
 int main(){
 pthread_t thread1,thread2;
-const char*message1="Thread 1";
-const char* message2="Thread 2;
 int res1=pthread_create(&thread1,NULL,print_message_function,(void*)message1);
 int res2=pthread_create(&thread2,NULL,print_message_function2,(void*)message2);
 int iret1,iret2;
